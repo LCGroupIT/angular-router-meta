e20a89789deebac1d82bf3b6ced8779b752eb287
Semenov.AA <a.semenov@lcgroup.su>
2018-10-29 16:46:32 +0300

    заменил двойные кавычки на одинарные

--- tests/router-meta.module.spec.ts
+++ tests/router-meta.module.spec.ts
@@ -1,4 +1,4 @@
-import { RouterTestingModule } from "@angular/router/testing";
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, inject, TestBed } from '@angular/core/testing';
 
 import { RouterMetaModule, RouterMetaService } from '../src';
